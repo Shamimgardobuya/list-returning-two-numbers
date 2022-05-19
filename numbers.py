adba7330faf9d9635996206e2573570499615d8a
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-19 22:47:58 +0300

    adding items to a list

---
+++ numbers.py
@@ -0,0 +1,13 @@
+
+
+# def highest(numbers):
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+p=[]
+p.append(a),p.append(b)
+# p.sort()
+print(p)
+# p=numbers  
+# highest(p=numbers)
+    
+    
